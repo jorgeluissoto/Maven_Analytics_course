1b7e66ca7ea249cc40b4ff32322c6e8e1f9c343b
Jorge Soto <jls@jorgeluissoto.com>
2023-02-17 23:45:46 -0500

    completed 6 section in analyzing multiple tables

--- MySQL/analyzing_multiple_tables_with_joins.sql
+++ MySQL/analyzing_multiple_tables_with_joins.sql
@@ -1,1 +1,20 @@
 -- Analyzing Multiple Tables 
+
+-- Inner Joins Example
+SELECT DISTINCT
+    inventory.inventory_id
+FROM
+    inventory
+        INNER JOIN
+    rental ON inventory.inventory_id = rental.inventory_id
+LIMIT 5000;
+
+/* Can you pull for me a list of each film we have in inventory?
+I would like to see the film's title, description, and the store_id value associated with each item,
+and its inventory_id.*/
+SELECT 
+    inventory_id, store_id, film.title, film.description
+FROM
+    inventory
+        INNER JOIN
+    film ON film.film_id = inventory.film_id;
