f60381799cd15217569abe2684d9fd8554704faa
Jorge Soto <jls@jorgeluissoto.com>
2023-02-18 17:26:38 -0500

    LEFT JOIN example and assignment

--- MySQL/analyzing_multiple_tables_with_joins.sql
+++ MySQL/analyzing_multiple_tables_with_joins.sql
@@ -1,6 +1,6 @@
 -- Analyzing Multiple Tables 
 
--- Inner Joins Example
+-- INNER JOIN Example
 SELECT DISTINCT
     inventory.inventory_id
 FROM
@@ -9,6 +9,7 @@ FROM
     rental ON inventory.inventory_id = rental.inventory_id
 LIMIT 5000;
 
+-- INNER JOIN ASSIGNMENT  
 /* Can you pull for me a list of each film we have in inventory?
 I would like to see the film's title, description, and the store_id value associated with each item,
 and its inventory_id.*/
@@ -18,3 +19,28 @@ FROM
     inventory
         INNER JOIN
     film ON film.film_id = inventory.film_id;
+
+-- LEFT JOIN Example
+SELECT DISTINCT
+    inventory.inventory_id, rental.inventory_id
+FROM
+    inventory
+        LEFT JOIN
+    rental ON inventory.inventory_id = rental.inventory_id
+LIMIT 5000;
+
+-- LEFT JOIN ASSIGNMENT 
+/*One of our investors is interested in the films we carry and how many actors are listed for each film title.
+Can you pull alist of all titles, and figures out how many actors are associated with each title?*/
+SELECT 
+    title, COUNT(actor_id) AS count_of_actors
+FROM
+    film
+        LEFT JOIN
+    film_actor ON film_actor.film_id = film.film_id
+GROUP BY title;
+
+-- RIGHT JOIN 
+
+
+
