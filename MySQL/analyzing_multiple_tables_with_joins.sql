15fb46474b78594609885689c42969322d4631a6
Jorge Soto <jls@jorgeluissoto.com>
2023-02-19 01:19:46 -0500

    complete all lessons for analyzing multiple tables with joins. Starting final project

--- MySQL/analyzing_multiple_tables_with_joins.sql
+++ MySQL/analyzing_multiple_tables_with_joins.sql
@@ -40,7 +40,56 @@ FROM
     film_actor ON film_actor.film_id = film.film_id
 GROUP BY title;
 
--- RIGHT JOIN 
+-- "Bridging" Unrelated Tables
+SELECT 
+    film.film_id, film.title, category.name AS category_name
+FROM
+    film
+        INNER JOIN
+    film_category ON film.film_id = film_category.film_id
+        INNER JOIN
+    category ON film_category.category_id = category.category_id;
 
+-- "Bridging" Assignment 
+/*Customer often ask which films their favorite actors appear in. It would be great to have a list of all actors,
+with each title that they apper in. Could you please pull that for me?*/
+SELECT 
+    actor.first_name, actor.last_name, film.title
+FROM
+    film
+        INNER JOIN
+    film_actor ON film.film_id = film_actor.film_id
+        INNER JOIN
+    actor ON actor.actor_id = film_actor.actor_id;
 
+-- Multi-Condition Join Assignment
+/*The manager from store 2 is working on expanding our film collection there.
+Could you pull a list of distict titles and their description, currently available in inventory at store 2?*/
+SELECT DISTINCT
+    film.title, film.description
+FROM
+    film
+        INNER JOIN
+    inventory ON film.film_id = inventory.film_id
+        AND inventory.store_id = 2;
 
+-- UNION Operator Example
+SELECT 
+    'advisor' AS type, first_name, last_name
+FROM
+    advisor 
+UNION SELECT 
+    'investor' AS type, first_name, last_name
+FROM
+    investor;
+
+/* We will be hosting a meeting with all our staff and advisor soon. Could you pull one list of all staff
+and advisor names, and include a column noting whether they are a staff member or advisor? Thank*/
+SELECT 
+    'advisor' AS type, first_name, last_name
+FROM
+    advisor 
+UNION SELECT 
+    'staff' AS type, first_name, last_name
+FROM
+    staff;
