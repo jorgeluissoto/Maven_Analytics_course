3438426a05dad8675a9be5ca01ba6df05570e04b
Jorge Soto <jls@jorgeluissoto.com>
2023-02-19 10:24:32 -0500

    completed 6/8 questions on final project

--- MySQL/MavenMovies_Final_Course_Project.sql
+++ MySQL/MavenMovies_Final_Course_Project.sql
@@ -3,51 +3,54 @@
 Please send over the managers’ names at each store, with the full address 
 of each property (street address, district, city, and country please).  
 */ 
+SELECT 
+    staff.first_name AS manager_first_name,
+    staff.last_name AS manager_last_name,
+    address.address,
+    address.district,
+    city.city,
+    country.country
+FROM
+    store
+        LEFT JOIN
+    staff ON store.manager_staff_id = staff.staff_id
+        LEFT JOIN
+    address ON store.address_id = address.address_id
+		LEFT JOIN 
+	city ON address.city_id = city.city_id
+		LEFT JOIN
+	country ON city.country_id = country.country_id;
 
-
-
-
-
-
-
-
-
-	
 /*
 2.	I would like to get a better understanding of all of the inventory that would come along with the business. 
 Please pull together a list of each inventory item you have stocked, including the store_id number, 
 the inventory_id, the name of the film, the film’s rating, its rental rate and replacement cost. 
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT 
+    inventory.store_id,
+    inventory.inventory_id,
+    film.title,
+    film.rating,
+    film.rental_rate,
+    film.replacement_cost
+FROM
+    inventory
+        LEFT JOIN
+    film ON film.film_id = inventory.film_id;
 
 /* 
 3.	From the same list of films you just pulled, please roll that data up and provide a summary level overview 
 of your inventory. We would like to know how many inventory items you have with each rating at each store. 
 */
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT 
+    inventory.store_id,
+    film.rating,
+    COUNT(inventory_id) AS inventory_items
+FROM
+    inventory
+        LEFT JOIN
+    film ON film.film_id = inventory.film_id
+GROUP BY inventory.store_id , film.rating;
 
 /* 
 4. Similarly, we want to understand how diversified the inventory is in terms of replacement cost. We want to 
@@ -55,34 +58,44 @@ see how big of a hit it would be if a certain category of film became unpopular 
 We would like to see the number of films, as well as the average replacement cost, and total replacement cost, 
 sliced by store and film category. 
 */ 
-
-
-
-
-
-
-
-
-
-
-
+SELECT 
+    inventory.store_id,
+    category.name AS category,
+    COUNT(film.film_id) AS films,
+    AVG(replacement_cost) AS average_replacement_cost,
+    SUM(replacement_cost) AS total_replacement_cost
+FROM
+    film
+        LEFT JOIN
+    film_category ON film.film_id = film_category.film_id
+        LEFT JOIN
+    category ON film_category.category_id = category.category_id
+        INNER JOIN
+    inventory ON film.film_id = inventory.film_id
+GROUP BY inventory.store_id , category.name
+ORDER BY SUM(film.replacement_cost) DESC;
 
 /*
 5.	We want to make sure you folks have a good handle on who your customers are. Please provide a list 
 of all customer names, which store they go to, whether or not they are currently active, 
 and their full addresses – street address, city, and country. 
 */
-
-
-
-
-
-
-
-
-
-
-
+SELECT 
+    customer.first_name,
+    customer.last_name,
+    customer.store_id AS store,
+    customer.active AS active_status,
+    address.address,
+    city.city,
+    country.country
+FROM
+    customer
+        LEFT JOIN
+    address ON customer.address_id = address.address_id
+        LEFT JOIN
+    city ON address.city_id = city.city_id
+        LEFT JOIN
+    country ON city.country_id = country.country_id;
 
 /*
 6.	We would like to understand how much your customers are spending with you, and also to know 
@@ -90,19 +103,27 @@ who your most valuable customers are. Please pull together a list of customer na
 lifetime rentals, and the sum of all payments you have collected from them. It would be great to 
 see this ordered on total lifetime value, with the most valuable customers at the top of the list. 
 */
+SELECT 
+    customer.customer_id,
+    customer.first_name,
+    customer.last_name,
+    COUNT(rental.rental_id) AS lifetime_rentals,
+    SUM(payment.amount) AS total_payments
+FROM
+    customer
+        LEFT JOIN
+    rental ON customer.customer_id = rental.customer_id
+        LEFT JOIN
+    payment ON payment.rental_id = rental.rental_id
+GROUP BY customer.customer_id
+ORDER BY total_payments DESC;
+-- used this to see the schema of the rental table to see how we need to group 
+SELECT 
+    *
+FROM
+    rental
+ORDER BY customer_id;
 
-
-
-
-
-
-
-
-
-
-
-
-    
 /*
 7. My partner and I would like to get to know your board of advisors and any current investors.
 Could you please provide a list of advisor and investor names in one table? 
