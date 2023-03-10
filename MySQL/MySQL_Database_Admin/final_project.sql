cd2dbd1fafaed1d0d311a0fdf244a75590f9de17
Jorge Soto <jls@jorgeluissoto.com>
2023-03-10 18:41:02 -0500

    completed final projects

--- MySQL/MySQL_Database_Admin/final_project.sql
+++ MySQL/MySQL_Database_Admin/final_project.sql
@@ -19,11 +19,22 @@ How should the tables relate to one another? */
 things like primary keys and foreign keys, and anythimg else you think you should have in a tables. Make sure to use
 reasonable data types for each column. */
 
+-- image of ERR is avalable on request 
+
 /* Create a schema called bubsbooties. Within that schema, create the tables that you have diagramed out. Make
 sure they relate to each other, and that they have the same reasonable data tyes you selected previously */
 
+-- created 4 tables using MySQL Workbench UI
+
+SELECT * FROM products;
+SELECT * FROM customers;
+SELECT * FROM employees;
+SELECT * FROM customer_purchases;
+
 /* Add any constraints you think your tables column should have. Think through which columns need to be unique,
 which ones are allowed to have NULL values, etc */
+
+-- added unique constrait to product_name 
 
 /* Insert at least records of data into each table. The exact values do not matter, so feel free to make them up.
 Just make sure that the data you insert makes sense, and that the tables tie together. */
