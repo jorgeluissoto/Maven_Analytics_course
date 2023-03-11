5699e8ebc8326384f06aba6b9600eaf43c1e54c9
Jorge Soto <jls@jorgeluissoto.com>
2023-03-10 21:34:38 -0500

    started advanced MySQL data analysis

--- MySQL/MySQL_Database_Admin/final_project.sql
+++ MySQL/MySQL_Database_Admin/final_project.sql
@@ -25,19 +25,63 @@ reasonable data types for each column. */
 sure they relate to each other, and that they have the same reasonable data tyes you selected previously */
 
 -- created 4 tables using MySQL Workbench UI
+USE bubsbooties;
 
-SELECT * FROM products;
-SELECT * FROM customers;
-SELECT * FROM employees;
-SELECT * FROM customer_purchases;
+SELECT 
+    *
+FROM
+    products;
+    
+SELECT 
+    *
+FROM
+    customers;
+    
+SELECT 
+    *
+FROM
+    employees;
+    
+SELECT 
+    *
+FROM
+    customer_purchases;
 
 /* Add any constraints you think your tables column should have. Think through which columns need to be unique,
 which ones are allowed to have NULL values, etc */
 
 -- added unique constrait to product_name 
+-- added unique constrait to the email so only one email per customer
 
 /* Insert at least records of data into each table. The exact values do not matter, so feel free to make them up.
 Just make sure that the data you insert makes sense, and that the tables tie together. */
 
+-- Product
+INSERT INTO products VALUES
+(1,'Mickey Ice Cream','2022-03-23'),
+(2, 'Mickey Ears', '2018-09-10'),
+(3,'Tron figure','2023-03-01');
+
+-- Customers
+INSERT INTO customers VALUES
+(1,'Jorge','Soto','jls@jorge.com'),
+(2, 'Gen','Perez','gen@gmail.com'),
+(3,'Joe','Smith','smith@gmail.com'); 
+
+-- employees
+INSERT INTO employees VALUES
+(1,'Mark','Adams','Cashier','2020-03-04'),
+(2,'John','Lee','Manager','2019-05-02'),
+(3,'John','Soto','Sales man','2021-09-07');
+
+-- Customer Puchases
+INSERT INTO customer_purchases VALUES
+(1,3,2,3,'2023-02-15','30.99'),
+(2,1,1,2,'2023-02-19','6.99'),
+(3,3,3,1,'2023-02-23','25.99');
+ 
+
 /* Create two users and give them access to the database. The first user, TuckerReilly, will be the DBA, and should 
 get full database admin privileges. The second user, EllaBrody, is an Analyst, and only needs read access. */
+
+-- completed in Admin dashboard 
