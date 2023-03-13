94a2a6a18c21efb4515649893e4ef1b66dba29f8
Jorge Soto <100888747+jorgeluissoto@users.noreply.github.com>
2023-03-13 18:32:52 -0400

    Update and rename MySQL_Advanced_Data_Analysis.sql to MySQL_Advanced_Data_Analysis_Traffic_Sources.sql
    completed all Analysis Traffic Sources lessons

--- MySQL/MySQL_Advanced_Data_Analysis_Traffic_Sources.sql
+++ MySQL/MySQL_Advanced_Data_Analysis_Traffic_Sources.sql
@@ -124,3 +124,35 @@ WHERE
 GROUP BY 1;
 
 -- 3.7% conversion on desktop compared to 0.9% on mobile. Increase bids on desktop 
+
+-- ASSIGNMENT: Trending w/ Gradular Segments
+/* After your device-level analysis of conversion rates, we realized desktop was doing well, so we bid our gserach
+nonbrand desktop campaigns up on 2012-05-19
+
+Could you pull weekly trends for both desktop and mobile so we can see the impact on volume?
+
+You can use 2012-04-15 until the bid changes as a baseline */
+-- used to see all date limit to 10 
+SELECT *
+FROM website_sessions
+LIMIT 10;
+
+SELECT 
+    MIN(DATE(created_at)) AS week_start,
+    COUNT(DISTINCT CASE
+            WHEN device_type = 'desktop' THEN website_session_id
+            ELSE NULL
+        END) AS dtop_sessions,
+	COUNT(DISTINCT CASE
+            WHEN device_type = 'mobile' THEN website_session_id
+            ELSE NULL
+        END) AS mob_sessions
+FROM
+    website_sessions
+WHERE
+    created_at < '2012-06-09'
+		AND created_at > '2012-04-15'
+        AND utm_source = 'gsearch'
+        AND utm_campaign = 'nonbrand'
+GROUP BY WEEK(created_at);
+-- He did see a pop in desktop sessions after 5/20 
